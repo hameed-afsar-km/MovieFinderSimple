c7f59a316e02377f81b83403378259b6e6a27332
Dartman-YT <106016291+Dartman-YT@users.noreply.github.com>
2025-07-13 22:47:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let card = document.querySelector(".movies-grid");
+let form = document.getElementById("searchForm");
+let text = document.getElementById("searchInput");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    // Clear previous results
+    card.innerHTML = "";
+
+    let API = `https://www.omdbapi.com/?s=${text.value}&apikey=d1c25dcd`;
+
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+        if (!data.Search) {
+            card.innerHTML = "<p>No movies found.</p>";
+            return;
+        }
+
+        data.Search.forEach(movie => {
+            // Create new elements for each movie
+            let div = document.createElement("div");
+            div.className = "movie-card";
+
+            let img = document.createElement("img");
+            img.src = movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/300x450?text=No+Image";
+            img.className = "movie-poster";
+
+            let name = document.createElement("h2");
+            name.textContent = movie.Title;
+            name.className = "movie-title";
+
+            let year = document.createElement("p");
+            year.textContent = movie.Year
+            year.className = "movie-year";
+
+            let desc = document.createElement("p");
+            desc.textContent = movie.Type.replace(/\b\w/g, c => c.toUpperCase());
+            desc.className = "movie-details";
+
+            // Append children
+            div.appendChild(img);
+            div.appendChild(name);
+            div.appendChild(year);
+            div.appendChild(desc);
+            card.appendChild(div);
+        });
+    });
+});
